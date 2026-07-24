6cde7774d6dff9632d7a658a606a54d4f3efda78
Script'Enjøyer <apexdevweb@gmail.com>
2026-07-24 18:35:50 +0200

    folio

--- assets/js/gsap/lenis_smooth.js
+++ assets/js/gsap/lenis_smooth.js
@@ -2,7 +2,7 @@ const lenis = new Lenis();
 lenis.on("scroll", ScrollTrigger.update);
 
 gsap.ticker.add((time) => {
-  lenis.raf(time * 270);
+  lenis.raf(time * 1000);
 });
 
 gsap.ticker.lagSmoothing(0);
